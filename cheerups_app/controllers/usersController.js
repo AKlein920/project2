e05cb36aa52388e5bcf827e588edf5728310883d
Abi Auslander <abi920@gmail.com>
2016-12-12 12:49:36 -0500

    edit, update, and delete routes for user are working.

--- cheerups_app/controllers/usersController.js
+++ cheerups_app/controllers/usersController.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var User = require('../models/users.js');
 var Cheerup = require('../models/cheerups.js');
 
-// index route
+// index route - show all users
 router.get('/', function(req, res) {
   User.find({}, function(err, foundUsers) {
     res.render('users/index.ejs', {
@@ -26,12 +26,35 @@ router.get('/new', function(req, res) {
   res.render('users/new.ejs');
 });
 
+// edit route
+router.get('/:id/edit', function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    res.render('users/edit.ejs', {
+      user: foundUser
+    });
+  });
+});
+
 // show route
 router.get('/:id', function(req, res) {
   User.findById(req.params.id, function(err, foundUser) {
     res.render('users/show.ejs', {
       user: foundUser
     });
+  });
+});
+
+// update route
+router.put('/:id', function(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body, function(err, foundUser) {
+    res.redirect('/users/'+req.params.id);
+  });
+});
+
+// delete route
+router.delete('/:id', function(req, res) {
+  User.findByIdAndRemove(req.params.id, function(err, foundUser) {
+    res.redirect('/users');
   });
 });
 
